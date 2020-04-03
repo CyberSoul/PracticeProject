6996cc348adae436315615152d52b0e1d4493420
Maksym Sliadniev <maksim.sliadniev@gmail.com>
2020-04-03 10:51:56 +0300

    + added movement

---
+++ PracticeProject/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player: MonoBehaviour
+{
+    [SerializeField] float m_speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vecrtical = Input.GetAxis("Vertical");
+
+        transform.Translate( new Vector3(horizontal, 0, vecrtical) * m_speed * Time.deltaTime);
+    }
+}
