05caad5fc524da1958a12da0a6b7572b8aee3447
Maksym Sliadniev <maksim.sliadniev@gmail.com>
2020-04-16 17:22:37 +0300

    +added example of lazy singleton and templaet singleton

---
+++ PracticeProject/Assets/Scripts/SingletonExample.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SingletonExample : MonoBehaviour
+{
+    private static SingletonExample m_instance;
+    public static SingletonExample Instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                //Debug.LogError("The SingletonExample is NULL");
+                //Lazy initiat on first call.
+                GameObject obj = new GameObject("SingletonExample");
+                obj.AddComponent<SingletonExample>();
+                
+                //Just my version: can instantiate from template.
+            }
+
+            return m_instance;
+        }
+    }
+
+    private void Awake()
+    {
+        m_instance = this;
+    }
+
+    public void SomeMethod()
+    {
+        //Do something.
+    }
+
+}
---
+++ PracticeProject/Assets/Scripts/SingletonTemplate.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class SingletonTemplate<T> : MonoBehaviour where T : SingletonTemplate<T>
+{
+    private static T m_instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (m_instance == null)
+            {
+                Debug.LogError($"Null instance for singleton of {typeof(T).ToString()}");
+                //GameObject obj = new 
+            }
+
+            return m_instance;
+        }
+    }
+
+    private void Awake()
+    {
+        m_instance = this as T; // (T)this
+
+        Init();
+    }
+
+    public void Init()
+    {
+    }
+}
